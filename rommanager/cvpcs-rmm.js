955546687542a2fba0d3d64f4f4a5e17679e8837
cvpcs <root@cvpcs.org>
2010-08-04 23:55:41 -0500

    changes for 1.0.0 being added to rom manager

--- rommanager/cvpcs-rmm.js
+++ rommanager/cvpcs-rmm.js
@@ -1,14 +1,250 @@
 {
 	version: 1,
-	homepage: "http://sapphire.cvpcs.org/",
-	donate: "https:/www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=B6J4MGS9ZH8WC",
+	homepage: "http://ccroms.net/",
+	donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=UXH6LPZL9E5XW&lc=US&item_name=CCRoms%20%28CVPCS%29&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted",
 	mirrors:
 	[
-		"http://sapphire.davekover.com/",
-		"http://www.mydroidfiles.com/sapphire/"
+		"http://files.ccroms.net/downloads.php?q="
+		"http://files.ccroms.net/downloads.php?m=mdw&q="
+		"http://files.ccroms.net/downloads.php?m=kover&q="
 	],
 	roms:
 	[
+	{
+		name: "Sapphire 1.0.0 (Froyo)",
+		summary: "Stable",
+		device: "sholes",
+		product: "Sapphire",
+		incremental: 1000,
+		modversion: "Sapphire-1.0.0-Droid",
+		file: "sapphire/froyo/1.0.0/release/sapphire-1.0.0-update.zip",
+		screenshots:
+		[
+		"http://imgur.com/0gWnc.png",
+		"http://imgur.com/yvyuH.png"
+		],
+		addons:
+		[
+		{
+			name: "Google Apps",
+			file: "sapphire/froyo/1.0.0/release/sapphire-1.0.0-gapps-update.zip"
+		},
+		{
+			name: "Enable Stagefright",
+			file: "sapphire/froyo/1.0.0/release/sapphire-1.0.0-stagefright-update.zip"
+		}
+		],
+		choices:
+		[
+		{
+			name: "Kernel",
+			options:
+			[
+			{
+				name: "cvpcs 7-slot 125-800 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-800LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-800 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-800SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-800 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-800IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-900 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-900LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-900 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-900SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-900 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-900IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1000 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1000LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1000 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1000SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1000 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1000IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1100 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1100LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1100 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1100SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1100 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1100IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1200 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1200LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1200 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1200SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1200 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1200IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1250 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1250LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1250 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1250SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1250 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1250IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1320 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1320LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1320 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1320SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 125-1320 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x125-1320IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-800 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-800LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-800 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-800SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-800 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-800IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-900 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-900LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-900 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-900SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-900 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-900IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1000 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1000LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1000 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1000SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1000 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1000IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1100 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1100LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1100 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1100SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1100 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1100IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1200 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1200LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1200 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1200SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1200 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1200IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1250 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1250LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1250 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1250SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1250 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1250IV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1320 mhz low voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1320LV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1320 mhz standard voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1320SV.zip"
+			},
+			{
+				name: "cvpcs 7-slot 250-1320 mhz increasing voltage",
+				file: "sapphire/froyo/1.0.0/kernels/latest/sholes_sapphire_7x250-1320IV.zip"
+			}
+			]
+		},
+		{
+			name: "Theme",
+			options:
+			[
+			{
+				name: "Stock (Default)",
+			},
+			{
+				name: "Black Bar",
+				file: "sapphire/froyo/1.0.0/themes/spr/sapphire-1.0.0-black_bar-theme.zip"
+			},
+			{
+				name: "Droid X",
+				file: "sapphire/froyo/1.0.0/themes/cw/sapphire-1.0.0-droid_x-theme-update.zip"
+			},
+			{
+				name: "Gala S",
+				file: "sapphire/froyo/1.0.0/themes/cw/sapphire-1.0.0-gala_s-theme-update.zip"
+			},
+			{
+				name: "Incognito",
+				file: "sapphire/froyo/1.0.0/themes/cw/sapphire-1.0.0-incognito-theme-update.zip"
+			},
+			{
+				name: "NexTang",
+				file: "sapphire/froyo/1.0.0/themes/cw/sapphire-1.0.0-nextang-theme-update.zip"
+			},
+			{
+				name: "NexTheme",
+				file: "sapphire/froyo/1.0.0/themes/cw/sapphire-1.0.0-nextheme-theme-update.zip"
+			}
+			]
+		}
+		]
+	},
 	{
 		name: "Sapphire 0.8.4 (Froyo)",
 		summary: "Stable",
@@ -16,7 +252,7 @@
 		product: "Sapphire",
 		incremental: 0840,
 		modversion: "Sapphire-0.8.4-Droid",
-		file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-update.zip",
+		file: "sapphire/froyo/0.8.4/sapphire-0.8.4-update.zip",
 		screenshots:
 		[
 		"http://i.imgur.com/2LpKS.png",
@@ -26,11 +262,11 @@
 		[
 		{
 			name: "Market Hack",
-			file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-market_hack-update.zip"
+			file: "sapphire/froyo/0.8.4/sapphire-0.8.4-market_hack-update.zip"
 		},
 		{
 			name: "Google Apps",
-			file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-gapps-update.zip"
+			file: "sapphire/froyo/0.8.4/sapphire-0.8.4-gapps-update.zip"
 		}
 		],
 		choices:
@@ -95,204 +331,23 @@
 			[
 			{
 				name: "Stock (Default)",
-				file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-stock-update.zip"
+				file: "sapphire/froyo/0.8.4/sapphire-0.8.4-stock-update.zip"
 			},
 			{
 				name: "Black Bar",
-				file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-black_bar-update.zip"
+				file: "sapphire/froyo/0.8.4/sapphire-0.8.4-black_bar-update.zip"
 			},
 			{
 				name: "NexTANG",
-				file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-nextang-update.zip"
+				file: "sapphire/froyo/0.8.4/sapphire-0.8.4-nextang-update.zip"
 			},
 			{
 				name: "Smoked Glass Blue",
-				file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-smoked_glass_blue-update.zip"
+				file: "sapphire/froyo/0.8.4/sapphire-0.8.4-smoked_glass_blue-update.zip"
 			},
 			{
 				name: "Smoked Glass Blue [NO FONTS]",
-				file: "files/sapphire/froyo/release/0.8.4/sapphire-0.8.4-smoked_glass_blue-nofonts-update.zip"
-			}
-			]
-		}
-		]
-	},
-	{
-		name: "Sapphire 0.7.0 (Froyo)",
-		summary: "Stable",
-		device: "sholes",
-		product: "Sapphire",
-		incremental: 0700,
-		modversion: "Sapphire-0.7.0-Droid",
-		file: "files/sapphire/froyo/release/0.7.0/sapphire-0.7.0-update.zip",
-		addons:
-		[
-		{
-			name: "Market Fix",
-			file: "files/sapphire/froyo/release/0.7.0/sapphire-0.7.0-market_fix-update.zip"
-		},
-		{
-			name: "Proprietaries (Voice Search/etc) Fix",
-			file: "files/sapphire/froyo/release/0.7.0/sapphire-0.7.0-prop_fix-update.zip"
-		},
-		{
-			name: "Smoked Glass (Blue) Theme",
-			file: "files/sapphire/froyo/release/0.7.0/sapphire-0.7.0-smoked_glass_blue-update.zip"
-		},
-		{
-			name: "Smoked Glass (Blue) Theme [NO FONTS]",
-			file: "files/sapphire/froyo/release/0.7.0/sapphire-0.7.0-smoked_glass_blue-NO_FONTS-update.zip"
-		}
-		],
-		choices:
-		[
-		{
-			name: "Kernel",
-			options:
-			[
-			{
-				name: "P3Droid's 125-800mhz Low Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-LV-800.zip"]
-			},
-			{
-				name: "P3Droid's 125-900mhz Low Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-LV-900.zip"]
-			},
-			{
-				name: "P3Droid's 125-1000mhz Low Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-LV-1000.zip"]
-			},
-			{
-				name: "P3Droid's 125-1100mhz Low Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-LV-1100.zip"]
-			},
-			{
-				name: "P3Droid's 125-1200mhz Low Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-LV-1200.zip"]
-			},
-			{
-				name: "P3Droid's 125-1250mhz Low Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-LV-1250.zip"]
-			},
-			{
-				name: "P3Droid's 125-800mhz Standard Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-800.zip"]
-			},
-			{
-				name: "P3Droid's 125-900mhz Standard Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-900.zip"]
-			},
-			{
-				name: "P3Droid's 125-1000mhz Standard Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-1000.zip"]
-			},
-			{
-				name: "P3Droid's 125-1100mhz Standard Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-1100.zip"]
-			},
-			{
-				name: "P3Droid's 125-1200mhz Standard Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-1200.zip"]
-			},
-			{
-				name: "P3Droid's 125-1250mhz Standard Voltage",
-				urls: ["http://www.p3designs.info/sapphire/p3droid-0.32.9-6-1250.zip"]
-			}
-			]
-		}
-		]
-	},
-	{
-		name: "Sapphire 0.5.2 (Eclair)",
-		summary: "Stable",
-		device: "sholes",
-		product: "Sapphire",
-		incremental: 0500,
-		modversion: "Sapphire-0.5.2-Droid",
-		file: "files/sapphire/eclair/release/0.5.2/sapphire-0.5.2-update.zip",
-		choices:
-		[
-		{
-			name: "Kernel",
-			options:
-			[
-			{
-				name: "Bekit-Sapphire Stock",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_stock.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 800mhz LV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x800LV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 800mhz SV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x800SV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1000mhz LV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1000LV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1000mhz SV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1000SV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1100mhz LV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1100LV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1100mhz IV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1100IV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1100mhz SV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1100SV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1200mhz LV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1200LV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1200mhz IV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1200IV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1200mhz SV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1200SV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1300mhz LV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1300LV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1300mhz IV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1300IV.zip"
-			},
-			{
-				name: "Bekit-Sapphire 7-slot 1300mhz SV",
-				file: "files/sapphire/eclair/release/kernels/bekit_sapphire_sholes_7x1300SV.zip"
-			}
-			]
-		},
-		{
-			name: "Theme",
-			options:
-			[
-			{
-				name: "Stock",
-				file: "files/sapphire/eclair/release/0.5.2/sapphire-0.5.2-stock-update.zip"
-			},
-			{
-				name: "Smoked Glass (Blue)",
-				file: "files/sapphire/eclair/release/0.5.2/sapphire-0.5.2-smoked_glass-update.zip"
-			},
-			{
-				name: "Drop",
-				file: "files/sapphire/eclair/release/0.5.2/sapphire-0.5.2-drop-update.zip"
-			},
-			{
-				name: "Luna",
-				file: "files/sapphire/eclair/release/0.5.2/sapphire-0.5.2-luna-update.zip"
+				file: "sapphire/froyo/0.8.4/sapphire-0.8.4-smoked_glass_blue-nofonts-update.zip"
 			}
 			]
 		}
