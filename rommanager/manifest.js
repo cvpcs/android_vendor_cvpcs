438813b26b1476ff0e9e306b47887ad6aaed00e0
cvpcs <root@cvpcs.org>
2010-11-28 19:52:58 -0600

    updated to match current RM manifests.js section

--- rommanager/manifest.js
+++ rommanager/manifest.js
@@ -1,11 +1,13 @@
-{
-	developer: "cvpcs",
-	id: "cvpcs",
-	summary: "Source-based mods made by CVPCS",
-	manifest: "http://sapphire.cvpcs.org/files/other/cvpcs-rmm.js",
-	free: true,
-	roms:
-	{
-		sholes: true
-	}
-}
+    {
+        developer: "cvpcs",
+        id: "cvpcs",
+        summary: "Source-based mods made by CVPCS",
+        manifest: "http://cvpcs.github.com/android_vendor_cvpcs/cvpcs-rmm.js",
+        free: true,
+        roms:
+        {
+            inc: true,
+            sholes: true,
+            supersonic: true
+        }
+    }
