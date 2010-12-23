9e97dd56740ae23a1b8bdc5a7574adba4b2d75c6
cvpcs <root@cvpcs.org>
2010-12-23 11:49:09 -0600

    holy shit are we version bumping for a 2.0 release?
    bump the changelog for 2.0.0 as the HEAD while we finish up the final touches
    on this here release

--- products/cvpcs_version.mk
+++ products/cvpcs_version.mk
@@ -1,6 +1,6 @@
 # set version numbers here, so they take effect across all builds for
 # all devices
 
-build_version_major := 1
-build_version_minor := 1
-build_version_revision := 8
+build_version_major := 2
+build_version_minor := 0
+build_version_revision := 0
